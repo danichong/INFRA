223042faaff29677f19caf68f26da4c381d15886
Daniela Choncen <nanyatori0209@gmail.com>
2026-04-22 01:58:10 +0000

    modificacion de 1 archivo

--- TAREA02/iac-lab02-main/iac/lambda.tf
+++ TAREA02/iac-lab02-main/iac/lambda.tf
@@ -23,3 +23,26 @@ data "archive_file" "example" {
   source_file = "../src/lambda/index.js"
   output_path = "../bin/lambda/function.zip"
 }
+
+# Lambda function
+resource "aws_lambda_function" "example" {
+  filename      = data.archive_file.example.output_path
+  function_name = "example_lambda_function"
+  role          = aws_iam_role.example.arn
+  handler       = "index.handler"
+  code_sha256   = data.archive_file.example.output_base64sha256
+
+  runtime = "nodejs20.x"
+
+  environment {
+    variables = {
+      ENVIRONMENT = "production"
+      LOG_LEVEL   = "info"
+    }
+  }
+
+  tags = {
+    Environment = "production"
+    Application = "example"
+  }
+}

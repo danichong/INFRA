9b32fbbdc2dbc87414509ea96d468f0733e21488
Daniela Choncen <nanyatori0209@gmail.com>
2026-04-22 01:56:27 +0000

    creacion de archivos

---
+++ TAREA02/iac-lab02-main/iac/lambda.tf
@@ -0,0 +1,25 @@
+# IAM role for Lambda execution
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "example" {
+  name               = "lambda_execution_role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+# Package the Lambda function code
+data "archive_file" "example" {
+  type        = "zip"
+  source_file = "../src/lambda/index.js"
+  output_path = "../bin/lambda/function.zip"
+}
